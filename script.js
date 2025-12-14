0a959e00538054acafc8d8125fe4804aba96c4fd
CaptainLeezee <liamjhennessy@gmail.com>
2025-12-14 17:41:19 +1030

    Refactor score processing and chart creation logic

--- script.js
+++ script.js
@@ -153,7 +153,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
             // --- 6. BUILD CHARTS ---
-            // createScoreChart(processedTeams, roundsCount); 
+            createScoreChart(processedTeams, roundsCount); 
             createWeeklyScoreChart(processedTeams, data.weeklyAverages, roundsCount);
 
         })
@@ -320,5 +320,6 @@ function createWeeklyScoreChart(teamsData, weeklyAverages, roundsCount) {
     loadSavedData();
 
 })();
+
 
 
